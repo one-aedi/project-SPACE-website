5ba0c1845e68fdcd26dca7f3f3bb09a5705ed643
one-aedi <adrian.dzurovcin@gmail.com>
2021-10-25 19:25:48 +0200

    version2.0

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ $('.navigation, .logo a')
 .hide()
 .fadeIn(2000);
 
-$(".container0").slideUp(0).delay(2000).fadeIn(400);
+$(".container0")
+.slideUp(0)
+.delay(2000)
+.fadeIn(400);
+
+$('.container03')
+.slideDown(2000);
+
+$(".navigation").on('mouseenter', function() {
+    
+
+});
+
+$('.forgot').on('click', function(){
+        alert('wtf');
+
+});
